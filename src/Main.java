d41f350df1d229adf38f7965bceb29996e04fea8
Ednei <edneitrabach@hotmail.com>
2024-02-01 17:53:18 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+
+
+        /*** if else
+        && ||
+        math
+        variaveis
+    }
+}
+
+/***
+ " "
+ ''
+ \n     pular linha
+ \t     tab
+ byte
+ short
+ int
+ long
+ char
+ float
+ double
+ boolean
+ ***/
+
